b6100ed684ead9be1d0839915277399260dea797
desarrollo-movil-idat <dam1.idat@gmail.com>
2026-03-26 08:19:43 -0500

    animación con pase a la actividad principal

--- app/src/main/java/pe/edu/idat/app_retrofit_persistence/view/SplashActivity.kt
+++ app/src/main/java/pe/edu/idat/app_retrofit_persistence/view/SplashActivity.kt
@@ -1,6 +1,9 @@
 package pe.edu.idat.app_retrofit_persistence.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +20,9 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 6000)
     }
 }
